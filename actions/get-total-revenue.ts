5bdd9b9d9855836503348c326729db504e306a39
navi-05 <naveenthatsmyname5@gmail.com>
2023-07-16 10:43:48 +0530

    Changed Type Errors

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -13,14 +13,14 @@ export const getTotalRevenue = async (storeId: string) => {
         }
       }
     }
-  })
+  });
 
-  const totalRevenue = paidOrders.reduce((total, order) => {
-    const orderTotal = order.orderItems.reduce((orderSum, item) => {
-      return orderSum + item.product.price.toNumber()
-    }, 0)
-    return total + orderTotal
-  }, 0)
+  const totalRevenue = paidOrders.reduce((total: any, order) => {
+    const orderTotal = order.orderItems.reduce((orderSum: any, item) => {
+      return orderSum + item.product.price.toNumber();
+    }, 0);
+    return total + orderTotal;
+  }, 0);
 
-  return totalRevenue
-}
+  return totalRevenue;
+};
